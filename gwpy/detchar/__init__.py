37b69fbb3722f2764e9f2905fce8a5a57caeee12
Duncan Macleod <duncan.macleod@ligo.org>
2015-03-24 17:48:02 -0500

    gwpy.detchar: first commit of working package
        - package has no contents

---
+++ gwpy/detchar/__init__.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+# Copyright (C) Duncan Macleod (2014)
+#
+# This file is part of the GWpy DetChar python package.
+#
+# GWpy DetChar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy DetChar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy DetChar.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The GWpy DetChar package (`gwpy.detchar`) provides python utilies for
+Gravitational-wave detector characterisation.
+
+This package extends the GWpy package for gravitational-wave
+data processing (https://gwpy.github.io).
+"""
+
+from . import version
+
+__version__ = version.version
+__author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
